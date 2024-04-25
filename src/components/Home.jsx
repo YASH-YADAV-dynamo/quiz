a0e0d0d3f78026f994e79307c65e82eb8ba48bce
YASH YADAV <147921735+YASH-YADAV-dynamo@users.noreply.github.com>
2024-04-25 04:00:20 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -160,7 +160,7 @@ return (
                 to="/student"
                 className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
               >
-                Login as Student
+                Attempt Quiz as student 
               </HashLink>
             </div>
             {/* Pricing Card */}
@@ -191,7 +191,7 @@ return (
                 to="/teacher"
                 className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
               >
-                Login as Teacher
+                Create Quiz as teacher 
               </HashLink>
             </div>
             {/* Pricing Card */}
